132687e189aab59d66ddc59955958bd6942d3866
spikeiverson <112721775+spikeiverson@users.noreply.github.com>
2023-04-20 08:47:34 -0700

    Add files via upload
    added some docstrings

--- Task Class.py
+++ Task Class.py
@@ -2,7 +2,18 @@
 
 class Task:
 
-    def __init__(self, course, title, time, priority, duedate):
+    def __init__(self, course, title, time, priority, duedate):             #please always refer to the title of the course as 
+                                                                            #COURSE rather than CLASS to avoid confusion
+        """
+        Creates a "Task" object, usually a homework assignment or an exam.
+        
+        Args:
+            course: (str) the name of the course the task belongs to
+            title: (str) title of the tast
+            time: (str) amount of time the task is estimated to take
+            priority: (int) urgency of the task on a scale from 1-5 (1 is lowest priority, 5 is highest)
+            duedate: (str) when the task is due, formatted as MM/DD"""
+
         self.course = course
         self.title = title
         self.time = time
@@ -10,16 +21,21 @@ class Task:
         self.duedate = duedate
 
     def get_course(self):
+        """Returns title of the course the task belongs to"""
         return self.course
     
     def get_title(self):
+        """Returns title of the task"""
         return self.title
     
     def get_time(self):
+        """Returns amount of time the task is estimated to take"""
         return self.time
     
     def get_priority(self):
+        """Returns a number from 1-5 indicating task's priority"""
         return self.priority
     
-    def due_date(self):
+    def get_duedate(self):
+        """Returns the task's due date"""
         return self.duedate
