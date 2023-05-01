42de31c260366ea6bec1597645c5c29822a1a8e5
luciaalday <123031285+luciaalday@users.noreply.github.com>
2023-05-01 16:50:54 -0700

    Update create_new_task.py

--- create_new_task.py
+++ create_new_task.py
@@ -29,39 +29,39 @@ new_task = tk.Tk()
 new_task.title("Create New Task")
 
 #title
-title_label = tk.Label(new_task, text="Title*:")
-title_label.grid(row=1, column=0)
+title_label = tk.Label(new_task, text="Title *")
+title_label.grid(row=1, column=0, sticky='w')
 
 title_entry = tk.Entry(new_task)
 title_entry.grid(row=1, column=1)
 
 #course
-course_label = tk.Label(new_task, text="Course*:")
-course_label.grid(row=2, column=0)
+course_label = tk.Label(new_task, text="Course *")
+course_label.grid(row=2, column=0, sticky='w')
 
 course_entry = tk.Entry(new_task)
 course_entry.grid(row=2, column=1)
 
 #due_date
-due_date_label = tk.Label(new_task, text="Due date*")
-due_date_label.grid(row=3, column=0)
+due_date_label = tk.Label(new_task, text="Due date *")
+due_date_label.grid(row=5, column=0, sticky='nw')
 
 due_date_entry = Calendar(new_task, selectmode='day', year=2023, month=5, day=1)
-due_date_entry.grid(row=3, column=1)
+due_date_entry.grid(row=5, column=1, sticky='w')
 
 #priority
-priority_label = tk.Label(new_task, text="Priority (5-1):")
-priority_label.grid(row=4, column=0)
+priority_label = tk.Label(new_task, text="Priority (5-1)")
+priority_label.grid(row=3, column=0, sticky='w')
 
 priority_entry = tk.Entry(new_task)
-priority_entry.grid(row=4, column=1)
+priority_entry.grid(row=3, column=1)
 
 #time
-time_label = tk.Label(new_task, text="Time:")
-time_label.grid(row=5, column=0)
+time_label = tk.Label(new_task, text="Time")
+time_label.grid(row=4, column=0, sticky='w')
 
 time_entry = tk.Entry(new_task)
-time_entry.grid(row=5, column=1)
+time_entry.grid(row=4, column=1)
 
 create_button = tk.Button(new_task, text="Create", command=create)
 create_button.grid(row=6, column=1)
